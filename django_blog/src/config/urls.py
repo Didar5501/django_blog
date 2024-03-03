6886d166b19455e7e675f423f584fe04532e0509
Didar5501 <didar5501@gmail.com>
2024-03-03 16:58:14 +0500

    ok

--- django_blog/src/config/urls.py
+++ django_blog/src/config/urls.py
@@ -1,21 +1,16 @@
-"""config URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from post import views 
+
+
+urlpatterns= [
+        path('admin/', admin.site.urls),
+        path('index/', views.index),
+        re_path(r'^about/$', views.about), # начало и конец
+        re_path(r'^g+', views.ggg), #1 или более раз
+        re_path(r'^contact(?:s)?/$', views.contacts), # повтор либо 0 или 1 раз 
+        re_path(r'^arсhive/\d{4}/$', views.arсhive),# ограничение кол-ва симоволов / цифр
+        path('home/', views.home),
+    ]
--- django_blog/src/post/views.py
+++ django_blog/src/post/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    print(request)
+    print(request.scheme)
+    print(request.path)
+    print(request.encoding)
+    return HttpResponse ('<h1>Hello World!</h1>')
+
+def about(request):
+    print('http://127.0.0.1:8000'+request.get_full_path())
+    return HttpResponse ('<a href="#"> About page!</a>') 
+
+def contacts(request):
+    return HttpResponse ('<h2>Contacts page</h2>') 
+
+def ggg(request):
+    return HttpResponse ('<h2> GGG page</h2>') 
+
+def arсhive(request):
+    return HttpResponse ('number page') 
+
+
+def home(request):
+    return HttpResponse('<h1>HOME</h1>')
